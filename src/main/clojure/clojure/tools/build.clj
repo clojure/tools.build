73a0dcfa91be03c7d4e0ed30deb1f6d2e791d16e
Alex Miller <alex.miller@cognitect.com>
2020-04-20 17:02:36 -0500

    work on sync-pom, include-resources, jar, javac

--- src/main/clojure/clojure/tools/build.clj
+++ src/main/clojure/clojure/tools/build.clj
@@ -18,58 +18,116 @@
     (-> basis :aliases key)
     key))
 
-(def default-params
-  "Build param defaults"
-  {:build/target-dir "target"
-   :build/clj-paths :clj-paths ; ["src"]
-   :build/java-paths :java-paths ; ["java" "src/main/java"]
-   :build/resource-dirs :resource-paths ; ["resources"]
-   :build/src-pom "pom.xml"})
-
-(defn build-params
-  [basis & param-srcs]
-  "Load build-params from param-srcs and merge those into basis under
-  :build/params. Each param-src can either be a deps alias or a map."
-  (println "param-srcs" param-srcs)
-  (let [params (map #(resolve-alias basis %) param-srcs)]
-    (update basis :build/params #(apply merge % params))))
+(defn build
+  "Execute build:
+     Load basis using project-deps (default=./deps.edn)
+     Load build params - either a map or an alias
+     Run tasks - task may have an arg map or alias, which is merged into the build params"
+  [{:keys [project-deps params tasks]}]
+  (let [{:keys [install-edn user-edn project-edn]} (reader/find-edn-maps)
+        ordered-edns (remove nil? [install-edn user-edn project-edn])
+        master-edn (deps/merge-edns ordered-edns)
+        basis (deps/calc-basis master-edn)
+        default-params (resolve-alias basis params)]
+    ;(println "params" params)
+    ;(println "default-params" default-params)
+    (reduce
+      (fn [flow [task-sym args]]
+        (let [resolved-task (if (qualified-symbol? task-sym)
+                              task-sym
+                              (symbol "clojure.tools.build.tasks" (str task-sym)))
+              _ (println "\nRunning task" (name resolved-task))
+              task-fn (requiring-resolve resolved-task)
+              arg-data (merge default-params (resolve-alias basis args) flow)
+              _ (clojure.pprint/pprint arg-data)
+              res (task-fn basis arg-data)]
+          (if-let [err (:error res)]
+            (throw (ex-info err {:task resolved-task, :arg-data arg-data}))
+            (merge flow res))))
+      nil
+      tasks)))
 
 (comment
-  (require '[clojure.tools.build.tasks :refer :all])
+  (require
+    '[clojure.tools.build.tasks :refer :all]
+    '[clojure.tools.build.extra :refer :all])
 
-  ;; basic clojure lib build
-  (->
-    (reader/find-edn-maps)
-    reader/order-edn-maps
-    deps/merge-edns
-    deps/calc-basis
-    (build-params default-params :build-info)
-    clean sync-pom jar end)
+  ;; Given aliases:
+  ;; :clj-paths ["src/main/clojure"]
+  ;  :java-paths ["java" "src/main/java"]
+  ;  :resource-paths ["resources"]
 
-  ;; javac, executable jar
-  (->
-    (reader/find-edn-maps)
-    reader/order-edn-maps
-    deps/merge-edns
-    deps/calc-basis
-    (build-params default-params :build-info {:build/main-class 'foo.Demo1})
-    clean javac jar end)
+  ;; clojure source lib
+  (build
+    '{:tasks [[clean] [sync-pom] [include-resources] [jar]]
+      :params {;; these could be defaulted as almost everyone needs these
+               :build/target-dir "target1"
+               :build/class-dir "target1/classes"
+               :build/resources :clj-paths
+               :build/src-pom "pom.xml"
 
-  ;; aot app jar
-  (->
-    (reader/find-edn-maps)
-    reader/order-edn-maps
-    deps/merge-edns
-    deps/calc-basis
-    (build-params default-params :build-info {:build/main-class 'clojure.tools.build.demo})
-    clean aot jar end)
+               ;; app specific
+               :build/lib my/lib1
+               :build/version "1.2.3"}})
+
+  ;; clojure source lib with git version template
+  (build
+    '{:tasks [[clean] [clojure.tools.build.extra/git-version] [sync-pom] [include-resources] [jar]]
+      :params {:build/target-dir "target2"
+               :build/class-dir "target2/classes"
+               :build/resources :rpaths
+               :build/src-pom "pom.xml"
+
+               :git-version/template "0.8.VERSION"
+               :git-version/version> :flow/version
+               :build/lib my/lib2
+               :build/version :flow/version}})
+
+  ;; java executable jar (no clojure!)
+  (build
+    '{:tasks [[clean] [javac] [sync-pom] [include-resources] [jar]]
+      :params {:build/target-dir "target3"
+               :build/class-dir "target3/classes"
+               :build/java-paths :java-paths ; ["java" "src/main/java"]
+               :build/resources :resource-paths ; ["resources"]
+               :build/src-pom "pom.xml"
+               :build/lib org.clojure/tools.build
+               :build/version "0.1.0"
+               :build/main-class clojure.tools.build.demo}})
+
+  ;; compiled clojure app jar
+  (build
+    '{:tasks [[clean] [compile-clj] [include-resources] [jar]]
+      :params {:build/target-dir "target4"
+               :build/class-dir "target4/classes"
+               :build/clj-paths :clj-paths ; ["src"]
+               :build/resources :resource-paths ; ["resources"]
+               :build/src-pom "pom.xml"
+               :build/lib org.clojure/tools.build
+               :build/version "0.1.0"
+               :build/main-class clojure.tools.build.demo}})
 
   ;; uber jar
-  (->
-    (reader/find-edn-maps)
-    reader/order-edn-maps
-    deps/merge-edns
-    deps/calc-basis
-    (build-params default-params :build-info)
-    clean sync-pom jar uber end)
+  (build
+    '{:tasks [[clean] [sync-pom] [include-resources] [jar] [uber]]
+      :params {:build/target-dir "target5"
+               :build/class-dir "target5/classes"
+               :build/resources :paths
+               :build/src-pom "pom.xml"
+               :build/lib my/lib1
+               :build/version "1.2.3"}})
+
+  ;; src lib AND compiled lib
+  (build
+    '{:tasks [[clean] [sync-pom] [jar]
+              [compile-clj] [jar {:build/classifier "aot"}]]
+      :params {:build/target-dir "target6"
+               :build/src-pom "pom.xml"
+               :build/lib org.clojure/tools.build
+               :git-version/version-template "0.8.VERSION"
+               :git-version/version :flow/version
+               :build/version :flow/version
+               :build/resource-dirs :resource-paths
+               :build/clj-paths :clj-paths}})
+
   )
--- src/main/clojure/clojure/tools/build/extra.clj
+++ src/main/clojure/clojure/tools/build/extra.clj
@@ -9,9 +9,8 @@
 (ns clojure.tools.build.extra)
 
 (defn git-version
-  [{:keys [params] :as build-info}]
-  (let [{:build/keys [version-template]} params
-        version (format version-template "123")] ;; use git to determine git distance
-    (println "Calculating git version:" version)
-    (assoc-in build-info [:build/params :version] version)))
-
+  [basis {version-template :git-version/version-template
+          flow-key :build/flow}]
+  (let [git-version "123" ;; TODO use git to determine git distance
+        version (format version-template git-version)]
+    {flow-key version}))
