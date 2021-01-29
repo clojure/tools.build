80886c559f99b55de7ca8e346aca1dcd4816c72f
Alex Miller <alex.miller@cognitect.com>
2021-01-29 10:46:42 -0600

    add uber example

--- build/build.clj
+++ build/build.clj
@@ -6,24 +6,40 @@
     [clojure.tools.build.tasks.clean :refer [clean]]
     [clojure.tools.build.tasks.sync-pom :refer [sync-pom]]
     [clojure.tools.build.tasks.copy :refer [copy]]
-    [clojure.tools.build.tasks.jar :as jar]))
+    [clojure.tools.build.tasks.jar :as jar]
+    [clojure.tools.build.tasks.uber :as uber]))
+
+(def defaults
+  {:build/lib 'my/lib1
+   :build/version "1.2.3"
+   :build/clj-paths :clj-paths
+   :build/copy-specs [{:from :clj-paths}]
+   :build/project-dir "."
+   :build/output-dir "."})
 
 (defn jar
   [opts]
   (let [basis (tbuild/load-basis (jio/file "deps.edn"))
-        params (merge {:build/lib 'my/lib1
-                       :build/version "1.2.3"
-                       :build/clj-paths :clj-paths
-                       :build/copy-specs [{:from :clj-paths}]
-                       :build/project-dir "."
-                       :build/output-dir "."}
-                 opts)
+        params (merge defaults opts)
         params (merge params (dirs basis params))]
     (clean basis params)
     (sync-pom basis params)
     (copy basis params)
     (jar/jar basis params)))
 
+(defn uber
+  [opts]
+  (let [basis (tbuild/load-basis (jio/file "deps.edn"))
+        params (merge defaults opts)
+        params (merge params (dirs basis params))]
+    (clean basis params)
+    (sync-pom basis params)
+    (copy basis params)
+    (jar/jar basis params)
+    (uber/uber basis params)))
+
+
 (comment
   (jar nil)
+  (uber nil)
   )
