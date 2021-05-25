76d5ba54aad6be88280012e5ec45b0e304213f22
Fogus <mefogus@gmail.com>
2021-05-25 10:28:13 -0400

    test copy tasks

---
+++ src/test/clojure/clojure/tools/build/tasks/test_copy.clj
@@ -0,0 +1,26 @@
+;   Copyright (c) Rich Hickey. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
+(ns clojure.tools.build.tasks.test-copy
+  (:require
+    [clojure.test :refer :all :as test]
+    [clojure.java.io :as jio]
+    [clojure.tools.build.api :as api]
+    [clojure.tools.build.test-util :refer :all]))
+
+(deftest test-copy
+  (with-test-dir "test-data/p1"
+    (api/set-project-root! (.getAbsolutePath *test-dir*))
+    ;; copy src into target, then clean, and check target dir is gone
+    (api/copy {:target-dir "target/classes"
+               :src-specs [{:src-dir "src" :include "**"}]})
+    (is (.exists (jio/file (project-path "target/classes/foo/bar.clj"))))))
+
+(comment
+  (run-tests)
+  )
